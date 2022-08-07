5b7b522bf091cedc39f21b099050f72cde255c26
seunghuibaek <bluewar111@nate.com>
2022-08-07 21:02:50 +0900

    첫번재 커밋

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <li
+      v-for="fruit in fruits"
+      :key="fruit">
+      {{ fruit }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  // data: function() {  -- function 생략 가능
+  data() {
+    return {
+      count: 0,
+			fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  methods: {
+    increase() {
+      this.count += 1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    font-size: 50px;
+    color: royalblue;
+  }
+	ul {
+		li {
+			
+		}
+	}
+</style>
